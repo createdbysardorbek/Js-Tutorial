feb1dbad782f7bd0b32afdc9aeb5779ffd9b78d8
Sardor <cr720051010@gmail.com>
2022-10-12 22:18:09 +0300

    Beginning files are added

---
+++ script.js
@@ -0,0 +1,9 @@
+let userCardNum = prompt('Karta raqamini kiriting:');
+// let userCardNumber = +userCardNum;
+let userEnteredNumLength = userCardNum.length;
+
+if (userEnteredNumLength == 16 && +userCardNum) {
+    alert('Karta raqamingiz qabul qilindi!');
+} else {
+    alert('Karta raqami kiritishda xatolik bor!');
+}
