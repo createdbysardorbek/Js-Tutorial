7523db50500829d69f6b243345d1c9b2b78baed2
Sardor <cr720051010@gmail.com>
2022-10-12 23:03:56 +0300

    PersonalMovieDataBase beginning script

---
+++ script.js
@@ -0,0 +1,15 @@
+let numberOfFilms = prompt("How many films have you seen?");
+let turnInto = +numberOfFilms;
+let lastFilm = prompt("The last film you have seen?");
+let markForMovie = prompt("How would you marked it?(from 1 to 10)");
+let personalMovieDB = {
+    count: turnInto,
+    movies: {
+        [lastFilm]: markForMovie,
+    },
+    actors: {},
+    genres: {},
+    privat: false,
+}
+
+console.log(personalMovieDB.movies);
