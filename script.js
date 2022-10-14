3d1e2e4d075237f7328ed1357cab8bb9942502f4
Sardor <cr720051010@gmail.com>
2022-10-14 18:17:54 +0300

    Movie database 2-part

--- script.js
+++ script.js
@@ -1,9 +1,36 @@
-let userCardNum = prompt('Karta raqamini kiriting:');
-// let userCardNumber = +userCardNum;
-let userEnteredNumLength = userCardNum.length;
+let movieDb = () => {
+    let numberOfFilms = +prompt("How many films have you seen?");
+    let personalMovieDB = {
+        count: numberOfFilms,
+        movies: {},
+        genres: {},
+        actors: [],
+        private: false,
+    }
 
-if (userEnteredNumLength == 16 && +userCardNum) {
-    alert('Karta raqamingiz qabul qilindi!');
-} else {
-    alert('Karta raqami kiritishda xatolik bor!');
+    if (personalMovieDB.count < 10) {
+        console.log("You are not very fond of films!");
+    } else if (personalMovieDB.count < 30) {
+        console.log("You are classic spectator!");
+    } else if (personalMovieDB.count > 30) {
+        console.log("You are fan of films!");
+    } else {
+        console.log("Failed!");
+    }
+
+    let i = 0;
+
+    while (i < 2) {
+        let lastMovie = prompt("Last movie you watched?"),
+            markForMovie = prompt("How do you rate it");
+        if ((lastMovie == '' && markForMovie == '') || (lastMovie == null && markForMovie == null) || (lastMovie == false && markForMovie == false)) {
+
+        } else {
+            personalMovieDB.movies[lastMovie] = markForMovie;
+            i++;
+        }
+    }
+    document.write(personalMovieDB.movies);
 }
+
+movieDb();
