c76904f829c436032d03d07eaddda432717b4c1e
Sardor <cr720051010@gmail.com>
2022-10-17 22:44:09 +0300

    Try with variables

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
-let userCardNum = prompt('Karta raqamini kiriting:');
-// let userCardNumber = +userCardNum;
-let userEnteredNumLength = userCardNum.length;
+// let userCardNum = prompt('Karta raqamini kiriting:');
+// // let userCardNumber = +userCardNum;
+// let userEnteredNumLength = userCardNum.length;
 
-if (userEnteredNumLength == 16 && +userCardNum) {
-    alert('Karta raqamingiz qabul qilindi!');
-} else {
-    alert('Karta raqami kiritishda xatolik bor!');
-}
+// if (userEnteredNumLength == 16 && +userCardNum) {
+//     alert('Karta raqamingiz qabul qilindi!');
+// } else {
+//     alert('Karta raqami kiritishda xatolik bor!');
+// }
+
+
+// const myName = {
+//     name: "Yusuf",
+// }
+// myName.name = "Sardor";
+// console.log(myName);
+
+// console.log(age);
+// let age = 17;
