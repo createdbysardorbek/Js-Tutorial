d7b8f9119afe160fa77c9007b750b9177aa61396
Sardor <cr720051010@gmail.com>
2022-10-18 14:22:04 +0300

    feat: Data Types: string, number, boolean, null, NaN, undefined, array, object

--- script.js
+++ script.js
@@ -1,4 +1,41 @@
-"use strict";
+// "use strict";
 
-number = 10;
-console.log(number);
+// number = 10;
+// console.log(number);
+
+
+// Data Type = Number
+let number = 5.6;
+let num = "Sardor" * 1; // NaN 
+console.log(8 / 0); // Infinity
+console.log(typeof num); // typeof NaN = number
+
+// String
+const userName = "Sardor";
+const clientName = `Sardor`;
+
+//Boolean
+const isMarried = false;
+const isCircleEarth = true;
+
+// Null
+// console.log(clientAge);
+
+// Undefined
+let undefined1;
+console.log(undefined1);
+
+// Object
+const thief = {
+    //key: value;  
+    age: 30,
+    height: 1.60,
+    hairColor: 'brown',
+};
+console.log(thief.hairColor);
+console.log(thief);
+
+// Array
+//                 0        1       2     3   4   5   6
+const colors = ["black", "blue", "green", [], 10, 13, {}];
+console.log(colors[6]);
