a4213bc16b20824ae2bece5b7e733750b89e7412
Sardor <cr720051010@gmail.com>
2022-10-18 13:19:59 +0300

    First commit

--- script.js
+++ script.js
@@ -1,27 +1,2 @@
-// let userCardNum = prompt('Karta raqamini kiriting:');
-// let userCardNumber = +userCardNum;
-// let userEnteredNumLength = userCardNum.length;
-
-// if (userEnteredNumLength == 16 && +userCardNum) {
-//     alert('Karta raqamingiz qabul qilindi!');
-// } else {
-//     alert('Karta raqami kiritishda xatolik bor!');
-// }
-
-
-// const myName = {
-//     name: "Yusuf",
-// }
-// myName.name = "Sardor";
-// console.log(myName);
-
-// console.log(age);
-// let age = 17;
-
-// function toBinary(n) {
-//     let b = n.toString(2);
-//     return b;
-// }
-
-// let resulr = toBinary(5);
-// alert(resulr);
+let a = 5;
+console.log(a);
