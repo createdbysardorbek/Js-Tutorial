edefd61aabceeaaeb268fd76f4db1f8e31666227
Sardor <cr720051010@gmail.com>
2022-10-20 18:14:26 +0300

    New checker

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const uzcardnumber = (num) => {
-    if (num.length !== 16 || isNaN(+num) || !num.startsWith(8600) || num.includes(' ')) {
+    if (num.length !== 16 || isNaN(+num) || !num.startsWith(8600) || num.includes(' ') || typeof num == 'null') {
         console.log("Invalid card number!");
     } else {
         let num2 = num.slice(4, 12);
