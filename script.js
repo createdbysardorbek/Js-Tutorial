a41b0518d329653a92c1974ff9ca2c205bb4a63b
Sardor <cr720051010@gmail.com>
2022-10-20 23:30:02 +0300

    Testing part two

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
-// Homework - 1
+// #1
 let footballPlayers = ['Cristiano Ronaldo', 'Karim Benzema', 'Vinisius Junior', 'Rodrygo Goes', 'Luka Modric', 'Fede Valverde', 'Marco Asensio', 'Thibaut Courtois', 'Toni Kroos', 'Sergio Ramos'];
 for (let i = 0; i < footballPlayers.length; i++) { console.log(`${i + 1}.${footballPlayers[i]}`); }
 
-// Homework - 2
+// #2
 let arr = ["Iphone", "Samsung", "Redmi", "Xiaomi", "Nokia"];
 function arrPush(item) {
     arr[(arr.length - 1) + 1] = item;
 }
-arrPush("Novey");
+// arrPush("Novey");
 
-// Homework - 3
+// #3
 function arrPop() {
-    arr.splice(arr.length - 1);
+    arr.splice((arr.length - 1), 1);
 }
 arrPop();
 
