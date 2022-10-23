888a93ec15c4761c958e15caf89554e9d6988afe
Sardor <cr720051010@gmail.com>
2022-10-23 19:36:29 +0300

    Correcting mistakes - 2

--- script.js
+++ script.js
@@ -19,28 +19,36 @@
 
 // #4
 
-let str = prompt("Enter the string:", "");
-let count = 0;
-let count2 = 0;
-let count3 = 0;
-function strCheck(str2) {
-    str2.toLowerCase;
-    let newStr = [...str2];
-    for (let i = 0; i < newStr.length; i++) {
-        if (newStr[i] === "x") {
-            count++;
-        } else if (newStr[i] == "o" || newStr[i] === '0') {
-            count2++;
-        } else {
-            count3++;
-        }
-    }
-    if (count === count2) {
-        console.log("True");
-    } else if (count3 > 0) {
-        console.log("Flase");
+for (let i = 0; i < 1; i++) {
+    let str = prompt("Enter the string:", "");
+    if (str === '' || str === null || str.includes(' ')) {
+        i--;
     } else {
-        console.log("False");
+        function strCheck(str2) {
+            let count = 0,
+                count2 = 0,
+                count3 = 0;
+            str2.toLowerCase;
+            let newStr = [...str2];
+            for (let i = 0; i < newStr.length; i++) {
+                if (newStr[i] === "x") {
+                    count++;
+                } else if (newStr[i] == "o" || newStr[i] === '0') {
+                    count2++;
+                } else {
+                    count3++;
+                }
+            }
+            if (count !== count2) {
+                console.log("False");
+            } else if (count3 > 0) {
+                console.log("False");
+            } else {
+                console.log("True");
+            }
+        }
+
+        strCheck(str);
     }
 }
-strCheck(str);
+
