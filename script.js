b8c4dfda7fcc442d37aa0c7390a6f6bb156df6e0
Sardor <cr720051010@gmail.com>
2022-10-23 18:51:40 +0300

    First homework

--- script.js
+++ script.js
@@ -1,18 +1,40 @@
-// #1
-let footballPlayers = ['Cristiano Ronaldo', 'Karim Benzema', 'Vinisius Junior', 'Rodrygo Goes', 'Luka Modric', 'Fede Valverde', 'Marco Asensio', 'Thibaut Courtois', 'Toni Kroos', 'Sergio Ramos'];
-for (let i = 0; i < footballPlayers.length; i++) { console.log(`${i + 1}.${footballPlayers[i]}`); }
+// // #1
+// let footballPlayers = ['Cristiano Ronaldo', 'Karim Benzema', 'Vinisius Junior', 'Rodrygo Goes', 'Luka Modric', 'Fede Valverde', 'Marco Asensio', 'Thibaut Courtois', 'Toni Kroos', 'Sergio Ramos'];
+// for (let i = 0; i < footballPlayers.length; i++) { console.log(`${i + 1}.${footballPlayers[i]}`); }
 
-// #2
-let arr = ["Iphone", "Samsung", "Redmi", "Xiaomi", "Nokia"];
-function arrPush(item) {
-    arr[(arr.length - 1) + 1] = item;
+// // #2
+// let arr = ["Iphone", "Samsung", "Redmi", "Xiaomi", "Nokia"];
+// function arrPush(item) {
+//     arr[(arr.length - 1) + 1] = item;
+// }
+// // arrPush("Novey");
+
+// // #3
+// function arrPop() {
+//     arr.splice((arr.length - 1), 1);
+// }
+// arrPop();
+
+// console.log(arr);
+
+// #4
+
+let str = prompt("Enter the string:", "");
+let count = 0;
+let count2 = 0;
+function strCheck(str2) {
+    let newStr = [...str2];
+    for (let i = 0; i < newStr.length; i++) {
+        if (newStr[i] === "x") {
+            count++;
+        } else if (newStr[i] === "o" || newStr[i] === '0') {
+            count2++;
+        }
+    }
+    if (count === count2) {
+        console.log("True");
+    } else {
+        console.log("False");
+    }
 }
-// arrPush("Novey");
-
-// #3
-function arrPop() {
-    arr.splice((arr.length - 1), 1);
-}
-arrPop();
-
-console.log(arr);
+strCheck(str);
